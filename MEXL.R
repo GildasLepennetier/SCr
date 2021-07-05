a576b2e4551443ff27d5b19229b9b61de4c82636
Lepennetier <GildasLepennetier@users.noreply.github.com>
2021-07-05 10:38:22 +0200

    Create MEXL.R

---
+++ MEXL.R
@@ -0,0 +1,10 @@
+MEXL<-function(){
+	for ( NAMEIDENT in NAMES_CLU ){
+		allMarkersExpr <- readRDS(paste0(SUBDIR,"/allMarkers.",NAMEIDENT,".Rds"))
+		Annotation <-read.xlsx(paste0(SUBDIR,"/CIPR.",NAMEIDENT,".xlsx"))
+		MERGED=merge.data.frame(allMarkersExpr,Annotation,by="cluster")
+		write.xlsx(MERGED,paste0(SUBDIR,"/allMarkers.",NAMEIDENT,"_CIPR.xlsx"))
+		rm(allMarkersExpr,Annotation,MERGED)
+		}
+	}
+}
