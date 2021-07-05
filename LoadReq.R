d324aaa259c300ac2c49af44debedf2bab380205
Lepennetier <GildasLepennetier@users.noreply.github.com>
2021-07-05 10:45:22 +0200

    Create LoadReq.R

---
+++ LoadReq.R
@@ -0,0 +1,30 @@
+library(limma) # For a more efficient implementation of the Wilcoxon Rank Sum Test
+library(Seurat)
+library(ggplot2) #ggplot
+library(ggrepel) #geom_label_repel() not working with Dimplot?
+library(dplyr) #%>%
+library(tidyr) #separate
+library(ggpubr) #grids
+library(openxlsx) # read.xlsx
+library(pheatmap) #pheatmap
+library(patchwork) # wrap_plots
+library(gtools) #mixedsort
+library(cowplot) # cowplot::plot_grid(gg1, gg2, gg3, nrow = nrow, align = "h", rel_widths = c(1, 1, 1))
+library(gt) # great table gt::gt
+library(Nebulosa) #plot_density (bioconductor) #https://bioconductor.org/packages/devel/bioc/vignettes/Nebulosa/inst/doc/nebulosa_seurat.html
+library(igraph) # used in cellchat
+library(CIPR)
+library(CellChat) # in case we saved objects: updateCellChat(Object)
+library(ggalluvial)
+library(harmony)
+library(openxlsx)
+library(gplots)
+library(SCENIC)
+	
+source("https://raw.githubusercontent.com/GildasLepennetier/R-stuff/master/not_in.R")
+source("https://raw.githubusercontent.com/GildasLepennetier/SCr/master/FAM.R")
+source("https://raw.githubusercontent.com/GildasLepennetier/SCr/master/ANN.R")
+source("https://raw.githubusercontent.com/GildasLepennetier/SCr/master/DOT.R")
+source("https://raw.githubusercontent.com/GildasLepennetier/SCr/master/MEXL.R")
+source("https://raw.githubusercontent.com/GildasLepennetier/SCr/master/CChat.R")
+source("https://raw.githubusercontent.com/GildasLepennetier/SCr/master/SCEN.R")
